d4e8b4e4384cb441c8574f86779eaef5205a54ce
Frizons <38401883+Frizons@users.noreply.github.com>
2024-10-21 14:28:50 +0200

    improve a lot, elumin page with widget is clean, card rework, site rework

--- pages/elumin.js
+++ pages/elumin.js
@@ -1,3 +1,11 @@
-<h1 class="normal-text" id="work-title">ELUMIN</h1>
-<p id="home-text"><strong>Elumin</strong> c'est un projet qui dure depuis 3 ans</p>
-<p id="home-text">Il à une page steam juste là : <a href="https://store.steampowered.com/app/2684580/Elumin/" target="_blank"><strong>Page Steam</strong></a></p>
+<div id="elumin-page">
+    <img id="logo-img" src="src\img\elumin_logo.png"></img> <img id="title-img" src="src\img\elumin_title.png"></img>
+    <p class="desc-text">Elumin is an exploration game in an energy plant on an isolated isle, you&#x27;re a mechanical engineer, your job is to do maintenance all around the site. When you arrive the 14 people team you&#x27;re supposed to find is nowhere to be found. The energy production sector can&#x27;t be turned off due to an unknown cause. From now on you&#x27;re the only one in charge of keeping operations stable. In the mean time exploring the site will grant you answers on what happened and why.</p>
+    <div class="screenshot-container">
+        <img class="screenshot-img" src="src\img\elumin_banner.png"></img>
+        <img class="screenshot-img" src="src\img\elumin_banner.png"></img>
+        <img class="screenshot-img" src="src\img\elumin_banner.png"></img>
+        <img class="screenshot-img" src="src\img\elumin_banner.png"></img>
+    </div>
+    <p><iframe src="https://store.steampowered.com/widget/2684580/" frameborder="0" class="widget-steam"></iframe></p>
+</div>
--- pages/home.js
+++ pages/home.js
@@ -3,7 +3,14 @@
     <p id="home-text">JE ME PRESENTE, VOILA</p>
     <p id="home-text">J'AI FAIT TOUT ÇA :</p>
     <div id="game-card-container">
-    <button value="/elumin" id="elumin-button" class="redirect-button game-card" onclick="route(), topFunction()">ELUMIN</button>
-    <button value="/firestage" class="redirect-button game-card" onclick="route(), topFunction()">FIRESTAGE</button>
+        <div class="game-card">
+            <img class="img-card" src="https://i.ibb.co/vD3zdTH/logo-full-res-centered-v1.png"></img>
+            <button value="/elumin" id="elumin-button" class="redirect-button card-button" onclick="route(), topFunction()">ELUMIN</button>
+        </div>
+        <div class="game-card">
+            <img class="img-card" src="https://i.ibb.co/ZM5Rs8f/icon-firestage.png"></img>
+            <button value="/firestage" id="firestage-button" class="redirect-button card-button" onclick="route(), topFunction()">FIRESTAGE</button>
+        </div>
+        
     </div>
 </div>
