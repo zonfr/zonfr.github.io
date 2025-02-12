d6c58b99befb7fde88c2266afd83883a75543c1d
Frizons <38401883+Frizons@users.noreply.github.com>
2025-02-12 18:17:21 +0100

    improving site, language system, command prompt on top

--- js/script.js
+++ js/script.js
@@ -19,7 +19,33 @@
 //   modal.style.display = "none";
 // }
 
+let lang_select = true;
+
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+function toggleLanguage() {
+  lang_select = !lang_select
+  if (lang_select){
+    switchLanguage("en")
+  }else{
+    switchLanguage("fr")
+  }
+}
+
+function switchLanguage(lang) {
+  let stylesheetUrl;
+  switch (lang) {
+    case 'en':
+      stylesheetUrl = 'css/en.css';
+      break;
+    case 'fr':
+      stylesheetUrl = 'css/fr.css';
+      break;
+    default:
+      stylesheetUrl = 'css/en.css';
+  }
+   document.getElementById('languageStylesheet').setAttribute('href', stylesheetUrl);
+  }
