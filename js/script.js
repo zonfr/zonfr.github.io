01d6b765e48cbaa829ad900b62fd7b5c5380a176
Frizons <38401883+Frizons@users.noreply.github.com>
2025-02-13 18:41:07 +0100

    added fully functional command line toy

--- js/script.js
+++ js/script.js
@@ -20,6 +20,8 @@
 // }
 
 let lang_select = true;
+const messages = [];
+let messageTyped = "";
 
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
@@ -27,11 +29,11 @@ function topFunction() {
 }
 
 function toggleLanguage() {
-  lang_select = !lang_select
+  lang_select = !lang_select;
   if (lang_select){
-    switchLanguage("en")
+    switchLanguage("en");
   }else{
-    switchLanguage("fr")
+    switchLanguage("fr");
   }
 }
 
@@ -49,3 +51,90 @@ function switchLanguage(lang) {
   }
    document.getElementById('languageStylesheet').setAttribute('href', stylesheetUrl);
   }
+
+function loadPage(){
+  switchLanguage('en');
+
+  setTimeout(function(){
+    addMessage(false, "Hey welcome to my site");
+  }, 1000);
+  setTimeout(function(){
+    addMessage(false, "I'm a french game developer");
+  }, 3000);
+  setTimeout(function(){
+    addMessage(false, "Scroll down to explore the things I've made :");
+  }, 6000);
+
+  document.addEventListener('keydown', function(evt) {
+    if (evt.code == "Backspace"){
+      eraseText();
+      return
+    }
+    if (evt.code == "Space"){
+      evt.preventDefault();
+      // topFunction();
+    }
+
+    if (evt.code == "Enter" || evt.code == "NumpadEnter"){
+      addMessage(true, messageTyped);
+      messageTyped = "";
+      document.getElementById('input-box').innerHTML = "";
+      const element = document.querySelector('#cmd-blink-bar');
+      element.style.visibility = "visible";
+    }else{
+      typeText(evt.key);
+    }
+    }, false);
+}
+
+function typeText(chr){
+  if (messageTyped.length > 40){
+    return
+  }
+  messageTyped += chr;
+  const element = document.querySelector('#cmd-blink-bar');
+  // element.style.marginLeft = (-340 + messageTyped.length * 4.98).toString() + 'px';
+  element.style.visibility = "hidden";
+  document.getElementById('input-box').innerHTML = messageTyped + "_";
+}
+function eraseText(){
+  messageTyped = messageTyped.substring(0, messageTyped.length-1);
+  const element = document.querySelector('#cmd-blink-bar');
+  // element.style.marginLeft = (-340 + messageTyped.length * 4.98).toString() + 'px';
+  document.getElementById('input-box').innerHTML = messageTyped + "_";
+  if (messageTyped == ""){
+    document.getElementById('input-box').innerHTML = "";
+    const element = document.querySelector('#cmd-blink-bar');
+    element.style.visibility = "visible";
+  }
+}
+
+function addMessage(visitor, mes){
+  //Push new message
+  if (visitor){
+    messages.unshift("<p lang='en' class='cmd-msg'> Visitor : " + mes + "</p>");
+  }else{
+    messages.unshift("<p lang='en' class='cmd-msg'> Zonfr : " + mes + "</p>");
+  }
+
+  //Clear Message out of bounds
+  if (messages.length > 8){
+    messages.pop();
+  }
+  
+  //Clear Command
+  if (mes == "clear" || mes == "clr"){
+    for (let i = 0; i < messages.length; i++){
+      messages.pop();
+    }
+    document.getElementById('message-box').innerHTML = "";
+    return;
+  }
+
+  //Displays Infos
+  document.getElementById('message-box').innerHTML = "";
+  for (let i = 0; i < messages.length; i++){
+    document.getElementById('message-box').innerHTML += messages[i];
+  }
+  
+}
