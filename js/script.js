64164e156abe604f60e8ed220b4e35f58b0c99a9
Frizons <38401883+Frizons@users.noreply.github.com>
2025-02-17 13:04:43 +0100

    metronim page added

--- js/script.js
+++ js/script.js
@@ -1,24 +1,3 @@
-// // Get the modal
-// var modal = document.getElementById('myModal');
-
-// // Get the image and insert it inside the modal - use its "alt" text as a caption
-// var img = document.getElementById('myImg');
-// var modalImg = document.getElementById("img01");
-// var captionText = document.getElementById("caption");
-// img.onclick = function(){
-//   modal.style.display = "block";
-//   modalImg.src = this.src;
-//   captionText.innerHTML = this.alt;
-// }
-
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
-
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//   modal.style.display = "none";
-// }
-
 const langs = {
   ENGLISH: 0,
   FRENCH: 1
@@ -26,11 +5,6 @@ const langs = {
 let lang_select = langs.FRENCH;
 let messages = [];
 let messageTyped = "";
-
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
 
 function toggleLanguage() {
   lang_select = (lang_select+1) % Object.keys(langs).length;
